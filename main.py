f3c33162f8b0b7904378e00278776a4fa41e01fd
ignacia <ignacia.rivas@alumnos.ucn.cl>
2023-04-02 18:47:11 -0400

    docs: add documentation to main

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from Connection import Connection
 
 etl = ETL()
 
+# Extraction of data from Excel
 table_sells = etl.extract("DatosEjemplo.xlsx", "Hoja1")
 table_salesmen = etl.extract("DatosEjemplo.xlsx", "Hoja2")
 table_products = etl.extract("DatosEjemplo.xlsx", "Hoja3")
 
+# Data transformation
 table_time = etl.transform_table_time(table_sells)
 table_salesmen = etl.transform_table_salesmen(table_salesmen)
 table_products = etl.transform_table_products(table_products)
-
 table_sells = etl.transform_table_sells(table_time, table_sells)
 
+# Establishing connection with database
 connection = Connection()
+# Creating tables and loading data into MySQL
 connection.create_tables()
 connection.load_data(table_salesmen,table_products, table_time, table_sells)
 
