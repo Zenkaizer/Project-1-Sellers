278b353814474d07cf23f91d825aa1ba74ac15f4
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2023-04-05 11:11:11 -0400

    refactor: Structure of the Connection class

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 from Connection import Connection
-from ETL import ETL
 
-etl = ETL()
-
-# Extraction of data from Excel
-table_sells = etl.extract("resources/DatosEjemplo.xlsx", "Hoja1")
-table_salesmen = etl.extract("resources/DatosEjemplo.xlsx", "Hoja2")
-table_products = etl.extract("resources/DatosEjemplo.xlsx", "Hoja3")
-
-# Data transformation
-table_time = etl.transform_table_time(table_sells)
-table_salesmen = etl.transform_table_salesmen(table_salesmen)
-table_products = etl.transform_table_products(table_products)
-table_sells = etl.transform_table_sells(table_time, table_sells)
-
-# Establishing connection with database
 connection = Connection()
 
-# Creating tables and loading data into MySQL
-connection.create_tables()
-connection.load_data(table_salesmen, table_products, table_time, table_sells)
+with open('scripts/star_schema.sql', 'r') as file:
+    for line in file.read().split(';'):
+        connection.execute(line)
+
+connection.close()
---
+++ scripts/star_schema.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS sells CASCADE;
+DROP TABLE IF EXISTS salesmen CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS time CASCADE;
+
+CREATE TABLE salesmen (
+  id             int(10) NOT NULL AUTO_INCREMENT,
+  representative varchar(255) NOT NULL UNIQUE,
+  region         varchar(255) NOT NULL,
+  id_region      int(10) NOT NULL,
+  email          varchar(255) NOT NULL UNIQUE,
+  last_name      varchar(255) NOT NULL,
+  contact_number int(10) NOT NULL,
+  PRIMARY KEY (id));
+CREATE TABLE products (
+  id           int(10) NOT NULL AUTO_INCREMENT,
+  product_code varchar(255) NOT NULL UNIQUE,
+  description  varchar(255) NOT NULL,
+  price        int(10) NOT NULL,
+  cost         int(10) NOT NULL,
+  PRIMARY KEY (id));
+CREATE TABLE time (
+  id           int(10) NOT NULL AUTO_INCREMENT,
+  `date`       date NOT NULL UNIQUE,
+  month_number int(10) NOT NULL,
+  month_name   varchar(255) NOT NULL,
+  year         int(10) NOT NULL,
+  day          int(10) NOT NULL,
+  PRIMARY KEY (id));
+CREATE TABLE sells (
+  id          int(10) NOT NULL AUTO_INCREMENT,
+  salesmen_id int(10) NOT NULL,
+  products_id int(10) NOT NULL,
+  time_id     int(10) NOT NULL,
+  units       int(10) NOT NULL,
+  profits     int(10) NOT NULL,
+  PRIMARY KEY (id));
+ALTER TABLE sells ADD CONSTRAINT FKsells33993 FOREIGN KEY (salesmen_id) REFERENCES salesmen (id);
+ALTER TABLE sells ADD CONSTRAINT FKsells815083 FOREIGN KEY (products_id) REFERENCES products (id);
+ALTER TABLE sells ADD CONSTRAINT FKsells43867 FOREIGN KEY (time_id) REFERENCES time (id);
