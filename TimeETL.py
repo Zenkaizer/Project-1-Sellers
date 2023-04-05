be9d64038e357ee592c7f80846f36974975f57f3
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2023-04-05 18:32:01 -0400

    docs: Docstring added

--- TimeETL.py
+++ TimeETL.py
@@ -4,26 +4,48 @@ import pandas
 class TimeETL:
     def __init__(self, connection):
         """
-
-        :param connection:
+        TimeETL class constructor
+        :param connection: Connection to MySQL database
         """
         self.dataframe = None
         self.connection = connection
 
-    def extract(self, name, sheet):
-        self.dataframe = pandas.read_excel(name, sheet_name=sheet)
+    def extract(self, path, sheet):
+        """
+        Read an Excel file for ETL process from time table
+        :param path: Excel file location
+        :param sheet: Excel file sheet name
+        :return: None
+        """
+        self.dataframe = pandas.read_excel(path, sheet_name=sheet)
 
     def __normalize_columns(self):
+        """
+        Rename the columns and removes that ones that are not used
+        :return: None
+        """
         self.dataframe.columns = ['date', 'representative', 'product_code', 'units']
         self.dataframe.drop(columns=['representative', 'product_code', 'units'], inplace=True)
 
     def __group_equal_columns(self):
+        """
+        Group only unique dates
+        :return: None
+        """
         self.dataframe.drop_duplicates(inplace=True)
 
     def __generate_month_number(self):
+        """
+        Create a column to store the month number from the date column
+        :return: None
+        """
         self.dataframe['month_number'] = self.dataframe['date'].dt.month
 
     def __generate_month_name(self):
+        """
+        Create a column to store the month name from the month number column
+        :return: None
+        """
         months = {
             1: 'Enero',
             2: 'Febrero',
@@ -42,12 +64,24 @@ class TimeETL:
         self.dataframe['month_name'] = self.dataframe['month_number'].map(months)
 
     def __generate_year(self):
+        """
+        Create a column to store the year from the date column
+        :return: None
+        """
         self.dataframe['year'] = self.dataframe['date'].dt.year
 
     def __generate_day(self):
+        """
+        Create a column to store the day from the date column
+        :return: None
+        """
         self.dataframe['day'] = self.dataframe['date'].dt.day
 
     def transform(self):
+        """
+        Execute all the transformations for the time table
+        :return: None
+        """
         self.__normalize_columns()
         self.__group_equal_columns()
         self.__generate_month_number()
@@ -56,6 +90,10 @@ class TimeETL:
         self.__generate_day()
 
     def load(self):
+        """
+        Load in time table
+        :return: None
+        """
         for row in self.dataframe.to_numpy():
             query = 'INSERT INTO time (date, month_number, month_name, year, day) VALUES (\'%s\', %s, \'%s\', %s, %s)'\
                     % (row[0], row[1], row[2], row[3], row[4])
